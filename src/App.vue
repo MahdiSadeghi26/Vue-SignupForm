d521f64def9c502fd834438970af9821b3bd54f3
Mehdi Sadeghi <mahdisadeghi1381m@gmail.com>
2023-07-12 19:53:27 +0330

    adding font layout and font-family

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
 
 <template>
   <div class="container">
-    
+    <div class="content">
+      <div class="leftSide">
+          
+      </div>
+      <div class="rightSide"></div>
+    </div>
   </div>
 </template>
 
@@ -12,5 +17,18 @@
   width: 100vw;
   height: 100vh;
   background: url('./images/bg-intro-desktop.png'),hsl(0, 100%, 74%) ;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .content{
+    width: 80%;
+    height: 90vh;
+    display: grid;
+    grid-template-columns: 50% 50%;
+    .leftSide{
+    }
+    .rightSide{
+    }
+  }
 }
 </style>
